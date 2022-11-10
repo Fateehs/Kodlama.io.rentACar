7c077d912a058cd62660fe0ae0992e8fe2046e1e
Fateehs <111433510+Fateehs@users.noreply.github.com>
2022-11-10 23:53:24 +0300

    Delete Devs directory

--- Devs/src/main/java/Kodlama/io/Devs/dataAccess/abstracts/SubTechnologyRepository.java
+++
@@ -1,9 +0,0 @@
-package Kodlama.io.Devs.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Kodlama.io.Devs.entities.concretes.SubTechnology;
-
-public interface SubTechnologyRepository extends JpaRepository<SubTechnology, Integer> {
-	SubTechnology getByName(String name);
-}
