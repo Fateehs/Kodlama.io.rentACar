411dda4ebe94059b015f6f6122763b7bf1c8ad45
Fatih Enes Selvi <benzer1010@gmail.com>
2022-11-10 23:45:24 +0300

    OneToMany Method added

--- Devs/src/main/java/Kodlama/io/Devs/entities/concretes/ProgrammingLanguage.java
+++ Devs/src/main/java/Kodlama/io/Devs/entities/concretes/ProgrammingLanguage.java
@@ -1,31 +1,37 @@
 package Kodlama.io.Devs.entities.concretes;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import Kodlama.io.Devs.entities.abstracts.EntityRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Table(name = "programming_languages")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class ProgrammingLanguage implements EntityRepository {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
+
+	@Column(name = "name")
 	private String name;
-	
-	public ProgrammingLanguage(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
+	@OneToMany(mappedBy = "programmingLanguage")
+	private List<SubTechnology> subtechnologies;
 }
